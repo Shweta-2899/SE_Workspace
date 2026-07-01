4b6c0770336e38741325c22b5c53a5f9957f795d
Shweta-2899 <shwetavadher28@gmail.com>
2026-07-01 12:19:51 +0530

    Add files via upload

---
+++ module 2/task 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n Shweta Rohan Chotaliya");
+    printf("\n Rajkot");
+    printf("\n 28/08/1999");
+    printf("\n BSc Chemistry");
+    getch();
+}
